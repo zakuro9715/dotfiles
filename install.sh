45a55d279d7dc104e697ef86374ae6c3a1abea17
Takahiro Yaota <zakuro@yuzakuro.me>
2015-11-14 20:41:09 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/zsh
+
+cat << EOS
+
+      DDDDDDDD      OOOOOOO   TTTTTTTTTTT   FFFFFFFFF  IIIIIIIIIII   LL         EEEEEEEEEE   SSSSSSS
+     DD      DD   OO      OO      TT       FF              II       LL         EE          SS
+    DD      DD   OO      OO      TT       FF              II       LL         EE            SS
+   DD      DD   OO      OO      TT       FFFFFFFFF       II       LL         EEEEEEEEEE       SS
+  DD      DD   OO      OO      TT       FF              II       LL         EE                 SS
+ DDDDDDDDD      OOOOOOO       TT       FF          IIIIIIIIIII  LLLLLLLLL  EEEEEEEEEE    SSSSSSS
+
+####################################################################################################
+EOS
+
 for f in home/.*
 do
   link="$HOME/${f##*/}"
   target="$(cd $(dirname $f) && pwd)/$(basename $f)"
 
+  echo "Removing $link"
   rm "$link" -r
 
+  echo "Creating $link"
   case ${OSTYPE} in
     msys*)
       link="%USERPROFILE%/${f##*/}"

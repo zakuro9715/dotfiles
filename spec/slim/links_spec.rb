717b9519f56015c58f7e313f9101000d40477d81
Takahiro Yaota <zakuro@yuzakuro.me>
2016-01-01 10:42:35 +0900

    Fix specs

--- spec/slim/links_spec.rb
+++ spec/slim/links_spec.rb
@@ -6,12 +6,3 @@ slim_files.each do |f|
     it { should be_linked_to f.targetpath }
   end
 end
-
-slim_link_basenames = slim_files.map(&:linkpath).map { |f| File.basename(f) }
-unneeded_files = full_files.reject { |f| slim_link_basenames.include?(File.basename(f)) }
-p unneeded_files
-unneeded_files.each do |f|
-  describe file(f.linkpath) do
-    it { should_not exist }
-  end
-end

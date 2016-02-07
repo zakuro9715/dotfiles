04f74f59e9c6320aaf58febf6f0fc2150650193c
Takahiro Yaota <zakuro@yuzakuro.me>
2016-02-07 00:10:52 +0000

    Add nvim config

--- install.zsh
+++ install.zsh
@@ -42,6 +42,13 @@ EOS
     target="$(cd $(dirname $f) && pwd)/$(basename $f)"
     create-symlink $link $target
   done
+
+  for f in xdg_home/*
+  do
+    link="${XDG_CONFIG_HOME:-$HOME/.config}/${f##*/}"
+    target="$(cd $(dirname $f) && pwd)/$(basename $f)"
+    create-symlink $link $target
+  done
 }
 
 install-slim() {

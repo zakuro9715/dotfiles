2f0c6edb75b688750d870cc52edfe5a630d74926
Takahiro Yaota <zakuro@yuzakuro.me>
2016-02-07 02:55:22 +0000

    Add bootstrap.sh

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,4 @@
+zsh install.zsh full
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zakuro9715/zakuroster.git ~/.oh-my-zsh/themes/zakuroster
+sh scripts/setup-vim-plugins.sh

e21ac0599604d0cc3f24a622147092b52abc9cc7
Takahiro Yaota <zakuro@yuzakuro.me>
2016-03-25 19:54:18 +0900

    bootstrap script に neobundle のインストールを追加

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,5 @@
 zsh install.zsh full
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zakuro9715/zakuroster.git ~/.oh-my-zsh/themes/zakuroster
+git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 sh scripts/setup-vim-plugins.sh

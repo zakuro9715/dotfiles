a0cf211282cc90811b0ae67cc025c80dea86d003
Takahiro Yaota <zakuro@yuzakuro.me>
2016-05-21 02:37:19 +0900

    tweak bootstraping

---
+++ bootstrap.zsh
@@ -0,0 +1,19 @@
+cat << EOS
+
+      DDDDDDDD      OOOOOOO   TTTTTTTTTTT   FFFFFFFFF  IIIIIIIIIII   LL         EEEEEEEEEE   SSSSSSS
+     DD      DD   OO      OO      TT       FF              II       LL         EE          SS
+    DD      DD   OO      OO      TT       FF              II       LL         EE            SS
+   DD      DD   OO      OO      TT       FFFFFFFFF       II       LL         EEEEEEEEEE       SS
+  DD      DD   OO      OO      TT       FF              II       LL         EE                 SS
+ DDDDDDDDD      OOOOOOO       TT       FF          IIIIIIIIIII  LLLLLLLLL  EEEEEEEEEE    SSSSSSS
+
+####################################################################################################
+EOS
+
+
+zsh scripts/install-symlinks
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zakuro9715/zakuroster.git ~/.oh-my-zsh/themes/zakuroster
+git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+sh scripts/setup-vim-plugins.sh

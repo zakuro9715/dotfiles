9082664cbcd8860d572969dd21d61844ee88c9fd
Takahiro Yaota <zakuro@yuzakuro.me>
2016-05-21 02:53:37 +0900

    Add log functions

--- home/.zsh/utils.zsh
+++ home/.zsh/utils.zsh
@@ -19,3 +19,23 @@ os() {
     echo "windows"
   fi
 }
+
+log-error() {
+  local message="ERROR: $1"
+  echo -e "\e[1;31m$message\e[m"
+}
+
+log-warning() {
+  local message="WARNING: $1"
+  echo -e "\e[;33m$message\e[m"
+}
+
+log-info() {
+  local message="INFO: $1"
+  echo $message
+}
+
+log-success() {
+  local message="$1"
+  echo -e "\e[1;32m$message\e[m"
+}

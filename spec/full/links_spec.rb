16c4b4927b1cad3422f11084b1acc29714ddb76d
Takahiro Yaota <zakuro@yuzakuro.me>
2016-05-21 02:19:13 +0900

    Remove specs

--- spec/full/links_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-full_files.each do |f|
-  describe file(f.linkpath) do
-    it { should be_symlink }
-    it { should be_linked_to f.targetpath }
-  end
-end
--- spec/slim/links_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-slim_files.each do |f|
-  describe file(f.linkpath) do
-    it { should be_symlink }
-    it { should be_linked_to f.targetpath }
-  end
-end

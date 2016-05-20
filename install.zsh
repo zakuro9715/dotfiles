eab9c9db124c80e21da58fa7691ebc78b47d0d1d
Takahiro Yaota <zakuro@yuzakuro.me>
2016-05-21 02:35:21 +0900

    Remove slim mode

--- install.zsh
+++ install.zsh
@@ -23,8 +23,7 @@ create-symlink() {
 }
 
 
-install-full() {
-  cat << EOS
+cat << EOS
 
       DDDDDDDD      OOOOOOO   TTTTTTTTTTT   FFFFFFFFF  IIIIIIIIIII   LL         EEEEEEEEEE   SSSSSSS
      DD      DD   OO      OO      TT       FF              II       LL         EE          SS
@@ -36,48 +35,16 @@ install-full() {
 ####################################################################################################
 EOS
 
-  for f in home/.[!.]*
-  do
-    link="$HOME/${f##*/}"
-    target="$(cd $(dirname $f) && pwd)/$(basename $f)"
-    create-symlink $link $target
-  done
+for f in home/.[!.]*
+do
+  link="$HOME/${f##*/}"
+  target="$(cd $(dirname $f) && pwd)/$(basename $f)"
+  create-symlink $link $target
+done
 
-  for f in xdg_home/*
-  do
-    link="${XDG_CONFIG_HOME:-$HOME/.config}/${f##*/}"
-    target="$(cd $(dirname $f) && pwd)/$(basename $f)"
-    create-symlink $link $target
-  done
-}
-
-install-slim() {
-  cat << EOS
-ddd   oo  ttttt ffff iiiii l     eeeee  sss
-d  d o  o   t   f      i   l     e     s
-d  d o  o   t   ffff   i   l     eeee   sss
-d  d o  o   t   f      i   l     e         s
-ddd   oo    t   f    iiiii lllll eeeee  sss   slim version
-----------------------------------------------------------
-EOS
-
-  for f in home/.*-slim*
-  do
-    link="$HOME/$(basename $f | sed s/-slim//)"
-    target="$(cd $(dirname $f) && pwd)/$(basename $f)"
-    create-symlink $link $target
-  done
-}
-
-
-if [[ $# -ne 1 ]]
-then
-  echo "Usage: zsh install.zsh TARGET"
-  exit 1
-fi
-
-case $1 in
-  full) install-full ;;
-  slim) install-slim ;;
-  *   ) echo "unknown target" ;;
-esac
+for f in xdg_home/*
+do
+  link="${XDG_CONFIG_HOME:-$HOME/.config}/${f##*/}"
+  target="$(cd $(dirname $f) && pwd)/$(basename $f)"
+  create-symlink $link $target
+done

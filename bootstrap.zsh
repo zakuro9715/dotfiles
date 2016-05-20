4e93426241275b8a4561c2de0305fb00d0252305
Takahiro Yaota <zakuro@yuzakuro.me>
2016-05-21 02:38:47 +0900

    Fix error on bootstrap.zsh

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -11,7 +11,7 @@ cat << EOS
 EOS
 
 
-zsh scripts/install-symlinks
+zsh "scripts/install-symlinks.zsh"
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zakuro9715/zakuroster.git ~/.oh-my-zsh/themes/zakuroster

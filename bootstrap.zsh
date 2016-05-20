c0e1197a65a5e98132f791b02e3dd55b3dd45513
Takahiro Yaota <zakuro@yuzakuro.me>
2016-05-21 02:40:05 +0900

    Tweak bootstrap.zsh

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -1,4 +1,5 @@
-cat << EOS
+show-welcome-message() {
+  cat << EOS
 
       DDDDDDDD      OOOOOOO   TTTTTTTTTTT   FFFFFFFFF  IIIIIIIIIII   LL         EEEEEEEEEE   SSSSSSS
      DD      DD   OO      OO      TT       FF              II       LL         EE          SS
@@ -9,10 +10,11 @@ cat << EOS
 
 ####################################################################################################
 EOS
+}
 
+show-welcome-mesgae
 
 zsh "scripts/install-symlinks.zsh"
-
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zakuro9715/zakuroster.git ~/.oh-my-zsh/themes/zakuroster
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim

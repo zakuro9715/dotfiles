62e0714483880096e4d9c15d34e797d4c1611f11
Takahiro Yaota <z@kuro.red>
2020-03-21 10:17:28 +0900

    Improve completion

--- home/.zsh/completion.zsh
+++ home/.zsh/completion.zsh
@@ -5,22 +5,15 @@ fpath=(
 )
 autoload -U compinit && compinit
 
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:default' menu select=2
+
+zstyle ':completion:*' completer _oldlist _complete _match _history _ignored _approximate _prefix
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z} r:|[._-]=*'
+
+setopt no_beep
+setopt no_match
 setopt list_packed
 setopt magic_equal_subst
 setopt auto_param_slash
 setopt mark_dirs
-
-# histroy completion
-autoload history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^p" history-beginning-search-backward-end
-bindkey "^n" history-beginning-search-forward-end
-
-setopt share_history
-setopt hist_ignore_dups  # Don't store a same command as last one.
-setopt hist_ignore_all_dups  # Romove same comands as command to store
-setopt append_history  # append not overwrite
-setopt extended_history
-setopt hist_ignore_space # Ignore if command is started with space
-setopt hist_save_no_dups

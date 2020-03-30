7a1eace95c1313792e4cdcbde17981d6e7fc0d83
z@kuro <z@kuro.red>
2020-03-30 22:42:07 +0900

    git 周りを整理

--- home/.zsh/alias.zsh
+++ home/.zsh/alias.zsh
@@ -1,13 +1,10 @@
 alias ls="ls --color=auto"
 alias l="ls"
-alias la="ls -a"
-alias lal="ls -al"
-alias ll="ls -l"
 
 alias mkdir="mkdir -p"
-mkcd() { mkdir $1 && cd $1 }
 
 alias rm="rm -r"
+alias cp="cp -R"
 
 alias bnudle="bundle"
 alias be="bundle exec"
@@ -16,11 +13,6 @@ alias berc="bundle exec rails c"
 alias bers="bundle exec rails s"
 
 alias dc="docker-compose"
-alias gi="git"
-alias nim="nvim"
-
-alias chown-me="sudo chown zakuro:zakuro"
-alias xmm="xmodmap ~/.xmodmap"
 
 if check-command colordiff
 then
--- home/.zsh/load-then-call.zsh
+++
@@ -1,4 +0,0 @@
-# Load script and call command e.g.) load-then-call ~/.zsh/commands.zsh build-with-make
-src=$1
-shift
-source $src && "$@"

8749f33487d48f030ed9d25e6df6e92c44e3ac12
z@kuro <z@kuro.red>
2020-05-11 00:32:15 +0900

    Remove install.zsh

--- install.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-if [[ ! $(which deno > /dev/null) ]]
-then
-    curl -fsSL https://deno.land/x/install/install.sh | sh
-fi
-echo deno -v
-deno --allow-env --allow-run --allow-read --allow-write bootstrap.ts

8097dfba1e4f6eccd47dd4474ec46833faf72daf
zakuro <z@kuro.red>
2020-10-04 06:58:31 +0900

    Fix docker-compose error on wsl

--- home/.zsh/midgard.zsh
+++ home/.zsh/midgard.zsh
@@ -1,1 +1,3 @@
 alias elm="'/mnt/c/Program Files (x86)/Elm/0.19/bin/elm.exe'"
+# https://qiita.com/smina2018/items/7e40ced3bfd9a727f384#docker-compose-%E5%AE%9F%E8%A1%8C%E6%99%82%E3%81%AB%E3%82%A8%E3%83%A9%E3%83%BC
+export PATH=$PATH:"/mnt/c/Program Files/Microsoft VS Code/bin:/mnt/c/Program Files/Docker/Docker/resources/bin"

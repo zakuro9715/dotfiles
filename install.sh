d3415b69129ed4e93a6c7905af0b503d0896efd6
zakuro <z@kuro.red>
2020-11-16 01:09:29 +0900

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if ! which zsh > /dev/null
+then
+  sudo apt install -yqq zsh
+fi
+
+if ! which git > /dev/null
+then
+  sudo apt install -yqq git
+fi
+
+zakuro="$HOME/src/github.com/zakuro9715"
+cd "$zakuro" || exit 1
+if ! [ -d "$zakuro/dotfiles" ]
+then
+  git clone git@github.com:zakuro9715/dotfiles
+fi
+cd "$zakuro/dotfiles" || exit 1
+zsh bootstrap.zsh

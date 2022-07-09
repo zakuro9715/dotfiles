84ae6c7bc8cdf8beb2e00333adab8739cbd688d7
zakuro <z@kuro.red>
2022-07-09 12:21:23 +0900

    update nvim instaler

--- installers/010-nvim.zsh
+++ installers/010-nvim.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
-#
+
+sudo apt update -yqq
+sudo apt install -yqq libfuse2
+
+mkdir -p "$HOME/bin"
 nvim="$HOME/bin/nvim"
 curl -Lo "$nvim" https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x "$nvim"

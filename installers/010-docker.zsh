3fb08e9c18c53d59c9c93ea0b678ca4227c389af
zakuro <z@kuro.red>
2022-07-09 12:21:02 +0900

    update docker installer

--- installers/010-docker.zsh
+++ installers/010-docker.zsh
@@ -1,6 +1,19 @@
 #!/usr/bin/env zsh
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-sudo apt install docker-ce
+# https://docs.docker.com/engine/install/ubuntu/
+
+sudo apt -yqq update
+sudo apt -yqq install \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get -yqq update
+sudo apt-get -yqq install docker-ce docker-ce-cli containerd.io docker-compose-plugin

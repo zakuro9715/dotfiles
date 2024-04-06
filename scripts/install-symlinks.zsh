74294c41db46e3c1fc625d84416b4973564d1bff
zakuro <z@kuro.red>
2024-04-06 09:33:36 +0900

    add home-script symlink

--- scripts/install-symlinks.zsh
+++ scripts/install-symlinks.zsh
@@ -41,6 +41,8 @@ do
   create-symlink $target $link
 done
 
+create-symlink "$(pwd)/home-scripts" "$HOME/scripts"
+
 if is_wsl
 then
   # TODO: powershell.exe Start-Process -Verb runas -FilePath powershell.exe install-symlinks.ps1
